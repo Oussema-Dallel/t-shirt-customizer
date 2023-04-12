c202db847b1a16eaaeac1e5248c89a8fe16a7e8a
Oussema Dallel <oussemadallel@gmail.com>
2023-04-12 03:01:36 +0200

    add TShirt textures and color

--- client/src/canvas/TShirt.tsx
+++ client/src/canvas/TShirt.tsx
@@ -1,13 +1,19 @@
+import { easing } from 'maath';
 import { globalState } from '../store';
+import { useFrame } from '@react-three/fiber';
 import { useSnapshot } from 'valtio';
 import { Decal, useGLTF, useTexture } from '@react-three/drei';
 import type { FunctionComponent, ReactElement } from 'react';
 
 const TShirt: FunctionComponent = (): ReactElement => {
-	const snap = useSnapshot(globalState);
+	const { color, isFullTexture, isLogoTexture, logoDecal, fullDecal } = useSnapshot(globalState);
 	const { nodes, materials } = useGLTF('./shirt_baked.glb');
-	const logoTexture = useTexture(snap.logoDecal);
-	const fullTexture = useTexture(snap.fullDecal);
+	const logoTexture = useTexture(logoDecal);
+	const fullTexture = useTexture(fullDecal);
+
+	useFrame((state, delta) => {
+		easing.dampC(materials.lambert1.color, color, 0.25, delta);
+	});
 
 	return (
 		<group>
@@ -20,7 +26,31 @@ const TShirt: FunctionComponent = (): ReactElement => {
 				// eslint-disable-next-line max-len
 				// eslint-disable-next-line react/no-unknown-property, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
 				material={ materials.lambert1 }
-			/>
+			>
+				{ isFullTexture
+					? (
+						<Decal
+							map={ fullTexture }
+							position={ [ 0, 0, 0 ] }
+							rotation={ [ 0, 0, 0 ] }
+							scale={ 1 }
+						/>
+					)
+					: null }
+				{ isLogoTexture && !isFullTexture
+					? (
+						<Decal
+							depthTest={ false }
+							depthWrite={ true }
+							map={ logoTexture }
+							map-anisotropy={ 16 }
+							position={ [ 0, 0.04, 0.15 ] }
+							rotation={ [ 0, 0, 0 ] }
+							scale={ 0.15 }
+						/>
+					)
+					: null }
+			</mesh>
 		</group>
 	);
 };
